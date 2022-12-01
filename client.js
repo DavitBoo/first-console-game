64e3355fef702817b0cb31acd67e7fc8fe7478d8
DavitBoo <davidmarras@gmail.com>
2022-12-01 13:07:40 +0100

    ready for heroku

--- client.js
+++ client.js
@@ -12,3 +12,7 @@ const socket = socketIoClient("http://127.0.0.1:3000")
 socket.on("your turn", async () => {
     let respone = await rl.question('It\' is your turn, please enter your next move: ')
   });
+
+socket.on('info', message => {
+    console.log(message)
+})
--- server.js
+++ server.js
@@ -7,7 +7,23 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, { /* options */ });
 
+let playerX;
+let playerO;
+
+
 io.on("connection", (socket) => {
+    if(playerX){
+        console.log('El segundo jugador de ha unido, comienza el juego...')
+        playerO = socket
+        //en vez de hablarle a una sola conxión(jugador), con io. emites a todos los sockets conectados
+        playerX.emit('info', 'Ya está aquí tu contrincante, comienza la partida...')
+        playerO.emit('info', 'Eres el segundo jugaro, la partida empieza ya...')
+    }else{
+        console.log('El primer jugador se ha unid, esperando al segundo...')
+        playerX = socket
+        playerX.emit('info', 'Eres el primer jugador, estamos esperando al segundo para dar comienzo a la partida...')
+    }
+
     console.log('A new player has joined!')
     socket.emit("your turn")
 });
